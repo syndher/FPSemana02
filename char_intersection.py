c5634afee48bb93011c8304873f9252d32a9f1b2
Miguel Filipe <miguelfilipe.dev@gmail.com>
2024-11-14 18:20:18 +0000

    changes to char intersection

--- char_intersection.py
+++ char_intersection.py
@@ -1,15 +1,13 @@
 palavra1 = input()
 palavra2 = input()
-set1 = {}
-set2 = {}
+conjunto = ""
 for letra in palavra1:
-    set1.update([letra])
+    set(palavra1)
 
 for letra in palavra2:
-    if letra in set1:
-        set2.update([letra])
-
-   
+    if letra in palavra1:
+        conjunto += letra
+        
 print(palavra1)
 print(palavra2)
-print(set2)
+print(conjunto)
